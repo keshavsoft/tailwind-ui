453e3c43fc466b317120bdb9492a59bf73d0df56
keshavsoft <nknnkumar@yahoo.com>
2026-05-25 10:49:21 +0530

    del

--- bin/v5/commands/header/steps/locateSource.js
+++ bin/v5/commands/header/steps/locateSource.js
@@ -9,9 +9,6 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const sourceVersion =
-    highestVersion;
-
 export const locateSource = () => {
     return path.join(
         __dirname,
