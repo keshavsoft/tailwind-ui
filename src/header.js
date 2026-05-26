c5cd56eda4102809b703330e8caf3afed30751db
keshavsoft <nknnkumar@yahoo.com>
2026-05-26 14:02:56 +0530

    public

--- src/header.js
+++ src/header.js
@@ -5,7 +5,7 @@
     window.KSHeaderTemplateVersion = "v1";
 
     const module = await import(
-        `../bin/header/${window.KSTableVersion}/commands/header/template/${window.KSHeaderTemplateVersion}/entry.js`
+        `../bin/header/${window.KSTableVersion}/commands/header/template/${window.KSHeaderTemplateVersion}/initHeader.js`
     );
 
     window.KSHeader = module.default;
