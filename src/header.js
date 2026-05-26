2b78ce91de311883116018b37da4927ac7259b15
keshavsoft <nknnkumar@yahoo.com>
2026-05-26 15:20:48 +0530

    head

--- src/header.js
+++ src/header.js
@@ -1,12 +1,25 @@
 // src/header.js
 
 (async () => {
+
     window.KSTableVersion = "v1";
     window.KSHeaderTemplateVersion = "v1";
 
-    const module = await import(
-        `../bin/header/${window.KSTableVersion}/commands/header/template/${window.KSHeaderTemplateVersion}/initHeader.js`
+    const modules = import.meta.glob(
+        "../bin/header/*/commands/header/template/*/initHeader.js"
     );
 
+    const modulePath =
+        `../bin/header/${window.KSTableVersion}/commands/header/template/${window.KSHeaderTemplateVersion}/initHeader.js`;
+
+    const loadModule = modules[modulePath];
+
+    if (!loadModule) {
+        throw new Error(`Module not found: ${modulePath}`);
+    }
+
+    const module = await loadModule();
+
     window.KSHeader = module.default;
+
 })();
