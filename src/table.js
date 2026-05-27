4ec8ab560ffd53f9384df9caa5b181ad833eb0a4
keshavsoft <nknnkumar@yahoo.com>
2026-05-27 18:31:55 +0530

    window object

--- src/table.js
+++ src/table.js
@@ -1,7 +1,9 @@
 import { initShowTable, initCreate, initTableWithFooter } from "../bin/table/v2/commands/table/template/v2/entry.js";
 
 (async () => {
-    window.KSVersion = "2.2.1";
+    window.KSVersion = "2.2.2";
+
+    window.KSTable = {};
 
     window.KSTable.initShowTable = initShowTable;
     window.KSTable.initCreate = initCreate;
