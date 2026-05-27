cb891206543756ecf0ff4d6d2494d69eecf13580
keshavsoft <nknnkumar@yahoo.com>
2026-05-27 18:19:42 +0530

    3 exposed

--- src/table.js
+++ src/table.js
@@ -1,7 +1,9 @@
-import { initShowTable } from "../bin/table/v2/commands/table/template/v2/entry.js";
+import { initShowTable, initCreate, initTableWithFooter } from "../bin/table/v2/commands/table/template/v2/entry.js";
 
 (async () => {
-    window.KSVersion = 2.2;
+    window.KSVersion = "2.2.1";
 
-    window.KSTable = initShowTable;
+    window.KSTable.initShowTable = initShowTable;
+    window.KSTable.initCreate = initCreate;
+    window.KSTable.initTableWithFooter = initTableWithFooter;
 })();
